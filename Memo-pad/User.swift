f70e781eac1fc335b60889834789178f68a9fadd
Yasunori Noguchi <ngssive1215@gmail.com>
2017-06-14 19:45:39 +0900

    First commit

---
+++ Memo-pad/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  Memo-pad
+//
+//  Created by Yasunori Noguchi on 2017/05/31.
+//  Copyright © 2017年 Yasunori Noguchi. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class User: Object {
+    dynamic var name = ""
+}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
