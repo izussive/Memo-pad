83c949cd4c74066815979021e9bd0b52e6bf7bbb
Yasunori Noguchi <ngssive1215@gmail.com>
2017-06-21 20:29:35 +0900

    saveを押した時にtitleをつけるAlertを表示させる

--- Memo-pad/MemoViewController.swift
+++ Memo-pad/MemoViewController.swift
@@ -16,19 +16,44 @@ class MemoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
     @IBAction func saveMemo(){
+        let alert = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .alert)
         
+        // OKボタンの設定
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            // OKを押した時入力されていたテキストを表示
+            if let textFields = alert.textFields {
+                
+                // アラートに含まれるすべてのテキストフィールドを調べる
+                for textField in textFields {
+                    print(textField.text!)
+                }
+            }
+        })
+        alert.addAction(okAction)
+        
+        // キャンセルボタンの設定
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        // テキストフィールドを追加
+        alert.addTextField(configurationHandler: {(textField: UITextField!) -> Void in
+            textField.placeholder = "Title"
+        })
+        
+        // アラートを画面に表示
+        self.present(alert, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
--- Memo-pad/ViewController.swift
+++ Memo-pad/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegte {
+    @IBOutlet var table:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //セルの数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //ID付きのセルを取得して、セル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "テスト"
+        return cell!
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegue(withIdentifier: "ToMemoView",sender: nil)
+    }
+    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "ToMemoView") {
+            let _: ViewController = (segue.destination as? ViewController)!
+        }
+    }
+    
 }
 
